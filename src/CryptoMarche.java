093ab13620c0bae710c48efa0ac3836dc02fd34d
Maxime <max.galmant@gmail.com>
2023-04-11 09:22:29 +0200

    Ajout de CryptoMarche, PorteFeuille et TestCryptoMarche

---
+++ src/CryptoMarche.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+public class CryptoMarche {
+
+    private ArrayList<Portefeuille> portefeuilles;
+    private static CryptoMarche marche;
+
+    private CryptoMarche(){
+        portefeuilles = new ArrayList<Portefeuille>();
+    }
+
+    public static CryptoMarche getInstance(){
+        if(marche == null){ marche = new CryptoMarche();}
+		return marche;
+    }
+
+    public void ajouter(Portefeuille p){
+        portefeuilles.add(p);
+    }
+
+    /**
+     * Cette fonction recherche sur le marché tous les portefeuilles 
+     * du propriétaire et calcule son capital en euros. 
+     * @param proprietare
+     * @return capital en euros du propriétare.
+     */
+    public double capitalEnEuros(String proprietaire){
+        /**
+			FONCTION À IMPLEMENTER
+        **/
+        
+        double capitalEnEuros = 0.0;
+        
+        for (Portefeuille p : portefeuilles)
+        {
+        	if (p.getProprietaire().equals(proprietaire))
+        	{
+        		capitalEnEuros += p.valeurEnEuros();
+        	}
+        }
+
+        return capitalEnEuros;
+    }
+
+    /**
+     * Cette fonction recherche sur le marché tous les portefeuilles 
+     * d'un type de devise et calcule le volume total de capital de 
+     * cette devise sur le marché 
+     * @param monnaie
+     * @return capital total en circulation de la cryptomonnaie (en euros).
+     */
+    public double capitalMonneaie(Cryptomonnaie monnaie){
+        /**
+			FONCTION À IMPLEMENTER
+        **/
+        
+        double capitalCrypto = 0.0;
+        
+        for (Portefeuille p : portefeuilles)
+        {
+        	if (p.getMonnaie().equals(monnaie))
+        	{
+        		capitalCrypto += p.valeurEnEuros();
+        	}
+        }
+
+        return capitalCrypto;
+
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        for(Portefeuille p : this.portefeuilles){
+            ret += p.toString() + "\n";
+        }
+        return ret;
+    }
+
+}
